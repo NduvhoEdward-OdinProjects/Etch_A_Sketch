e867601dae00aac6d78d2a40b8ff88979690aa11
NduvhoEdward <71088485+NduvhoEdward@users.noreply.github.com>
2023-03-10 00:24:28 +0200

    Add gridsize request button

--- js/etch_a_sketch.js
+++ js/etch_a_sketch.js
@@ -6,16 +6,35 @@ const main = document.querySelector('main');
 main.appendChild(gridContainer);
 
 // Grids 
-const gridSize = 16;
+let gridSize = 16;
 let blocksRow = null; 
 
-for (let i = 0; i < gridSize; i++) { 
-    blocksRow = document.createElement('div');
-    blocksRow.classList.add('blocksRow');
-    for (let j = 0; j < gridSize; j++) { 
-        let block = document.createElement('div'); 
-        block.classList.add('block');
-        blocksRow.appendChild(block); 
-    }
-    gridContainer.appendChild(blocksRow);
+function drawGrid() {
+	for (let i = 0; i < gridSize; i++) { 
+	    blocksRow = document.createElement('div');
+	    blocksRow.classList.add('blocksRow');
+	    for (let j = 0; j < gridSize; j++) { 
+	        let block = document.createElement('div'); 
+	        block.classList.add('block');
+	        blocksRow.appendChild(block); 
+	    }
+	    gridContainer.appendChild(blocksRow);
+	}
 }
+
+drawGrid();
+
+function clearGrid() {
+	while (gridContainer.firstChild) {
+		gridContainer.removeChild(gridContainer.firstChild);
+	}
+}
+
+
+// Grid size button 
+let getGridSize = document.getElementById("get-grid-size");
+getGridSize.addEventListener("click", function() {
+	gridSize = parseInt(prompt("Please enter your desired grid size:"));
+	clearGrid()
+	drawGrid();
+});
