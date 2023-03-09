7c4788e024a9a483436616e481d6b6314028697a
NduvhoEdward <71088485+NduvhoEdward@users.noreply.github.com>
2023-03-09 21:21:27 +0200

    Add the grid blocks into the container

--- js/etch_a_sketch.js
+++ js/etch_a_sketch.js
@@ -1,3 +1,4 @@
+// Greetings msg
 const greeting = document.createElement('div');
 greeting.classList.add('greeting');
 greeting.textContent = 'Shall we begin?';
@@ -5,11 +6,24 @@ greeting.textContent = 'Shall we begin?';
 const header = document.querySelector('header');
 header.appendChild(greeting); 
 
+// Grid container
 const gridContainer = document.createElement('div');
 gridContainer.classList.add('grid-container');
 
-
-
-
 const main = document.querySelector('main');
 main.appendChild(gridContainer);
+
+// Grids 
+const gridSize = 8;
+let blocksRow = null; 
+
+for (let i = 0; i < gridSize; i++) { 
+    blocksRow = document.createElement('div');
+    blocksRow.classList.add('blocksRow');
+    for (let j = 0; j < gridSize; j++) { 
+        let block = document.createElement('div'); 
+        block.classList.add('block');
+        blocksRow.appendChild(block); 
+    }
+    gridContainer.appendChild(blocksRow);
+}
